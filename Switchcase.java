3022c6522d1d705c7741b1a1b5f641fc68c976fe
Neha Fartiyal <1000024339@dit.edu.in>
2025-05-29 20:49:06 +0530

    Create Switchcase.java

---
+++ Switchcase.java
@@ -0,0 +1,20 @@
+public class switchcase {
+    public static void main(String[] args) {
+      int item = 2;
+        switch(item){
+            case 1:
+                System.out.println("prize is 100");
+            break;
+            case 2:
+            System.out.println("prize is 200");
+            break;
+            case 3:
+            System.out.println("prize is 400");
+            break;
+            default:
+                System.out.println("invalid");
+
+        }
+    }
+    
+}
