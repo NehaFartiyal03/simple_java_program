b11264edc385c6c31d5c684cf99ab79de5069009
Neha Fartiyal <1000024339@dit.edu.in>
2025-05-29 20:47:47 +0530

    Create PrintDigit.java

---
+++ PrintDigit.java
@@ -0,0 +1,19 @@
+public class printdigit {
+    public static void main(String[] args) {
+        int num = 567398;
+        int cpy = num;
+        int count = 0;
+        while(cpy!=0) {
+            count++;
+            cpy = cpy/10;
+        }
+        while(num!=0){
+            int pow = (int)Math.pow(10, count-1);
+            System.out.println(num/pow);
+            num = num % pow;
+            count--;
+        }
+        
+    }
+    
+}
